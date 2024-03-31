e1254a83f50d363393ba488cf8f9e3afc3a0ab25
Mooneunjun <mooneunjun@icloud.com>
2024-03-31 17:06:23 +0900

    no message

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,36 @@
 import ReviewList from "./ReviewList";
-import { useState } from "react";
-import { getReviews } from "./api";
+import { getReviews } from "../api";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [order, setOrder] = useState("createdAt");
   const [items, setItems] = useState([]);
-  const [order, setOrder] = useState("");
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewestClick = () => setOrder("createdAt");
-  const hanleBestClick = () => setOrder("rating");
+  const handleBestClick = () => setOrder("rating");
 
   const handleDelete = (id) => {
     const nextItems = items.filter((item) => item.id !== id);
     setItems(nextItems);
   };
 
-  const handleLoadClick = async () => {
-    const { reviews } = await getReviews();
+  const handleLoad = async (orderQuery) => {
+    const { reviews } = await getReviews(orderQuery);
     setItems(reviews);
   };
 
+  useEffect(() => {
+    handleLoad(order);
+  }, [order]);
+
   return (
     <div className="App">
-      <button onClick={handleNewestClick}>최신순</button>
-      <button onClick={hanleBestClick}>베스트순</button>
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleBestClick}>베스트순</button>
+      </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
-      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
---
+++ test/index.js
@@ -0,0 +1,25 @@
+const myNumberTag = document.getElementById("myNumber");
+const decreaseBtn = document.getElementById("decrease");
+const increaseBtn = document.getElementById("increase");
+
+let myNumber = +myNumberTag.textContent;
+
+decreaseBtn.onclick = function () {
+  if (myNumber > 0) {
+    myNumber--;
+    myNumberTag.textContent = myNumber;
+  }
+};
+
+increaseBtn.onclick = function () {
+  myNumber++;
+  myNumberTag.textContent = myNumber;
+};
+
+const colorBtns = document.getElementsByClassName("color-btn");
+
+for (let btn of colorBtns) {
+  btn.onclick = function () {
+    myNumberTag.style.color = btn.dataset.color;
+  };
+}
